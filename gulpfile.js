973689a84158dc0c95beebcbf424b0eb7f281389
Andres <andrewplanet@gmail.com>
2015-06-23 22:10:25 -0430

    moar files

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require("gulp");
+		sass = require("gulp-ruby-sass")
+		size = require("gulp-size");
+		rename = require("gulp-rename");
+		minify = require("gulp-minify-css");
+		browserSync = require("browser-sync");
+		prefix = require("gulp-autoprefixer");
+		reload = browserSync.reload;
+
+var csspaths = {
+	src: "./sass/app.sass",
+	dest: "./css/"
+}
+
+gulp.task("bs", function(){
+	browserSync({
+		server: {
+			baseDir: "./"
+		}
+	})
+});
+
+gulp.task("bs-reload", function(){
+	browserSync.reload();
+})
+
+gulp.task("sass", function(){
+	sass(csspaths.src, {style: "expanded"})
+		.on("error", function(error){console.log(error.message)})
+		.pipe(prefix())
+		.pipe(gulp.dest(csspaths.dest))
+		.pipe(size({showFiles: true}))
+		.pipe(reload({stream: true}));
+})
+
+gulp.task("css-production", function(){
+	gulp.src(csspaths.dest + "app.css") //WARNING
+		.pipe(minify())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest(csspaths.dest))
+		.pipe(size({showFiles: true, gzip: true}))
+		.pipe(reload({stream: true}));
+})
+
+gulp.task("production", ["css-production"])
+
+gulp.task("default", ["bs", "sass"], function(){
+	gulp.watch("sass/**/*.sass", ["sass"]);
+	gulp.watch("*.html", ["bs-reload"]);
+})
